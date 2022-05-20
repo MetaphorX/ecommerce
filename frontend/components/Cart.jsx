5481517424331d02f39807eec9f775e4387671c4
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-05-20 16:51:00 +0100

    Add files via upload

--- frontend/components/Cart.jsx
+++ frontend/components/Cart.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import Link from 'next/link'
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai'
+import {TiDeleteOutline} from 'react-hot-toast/ti'
+import {toast} from 'react-hot-toast'
+import { useStateContext } from '../context/StateContext'
+import { urlFor } from '../lib/client'
+
 
 const Cart = () => {
+
+  const cartRef = useRef()
+  const {totalPrice, totalQuantities, cartItems, setShowCart} = useStateContext()
+
   return (
-    <div>Cart</div>
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className="cart-container">
+        <button 
+          type="button" 
+          className="cart-heading"
+          onClick={() =>setShowCart(false)}
+          >
+            <AiOutlineLeft />
+            <span>Your cart</span>
+            <span>{totalQuantities}</span>
+          </button>
+      </div>
+    </div>
   )
 }
 
