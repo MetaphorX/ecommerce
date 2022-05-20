e0430454ad66034e5f40e0a73cd59e85a9c65902
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-05-20 11:02:28 +0100

    Add files via upload

--- frontend/components/HeroBanner.jsx
+++ frontend/components/HeroBanner.jsx
@@ -5,7 +5,9 @@ import { urlFor } from '../lib/client'
 const HeroBanner = ({heroBanner:{smallText, product, largeText1, image, desc, buttonText, midText}}) => {
   
     return (
+        
     <div className='hero-banner-container'>
+        
         <p className="beats-solo">{smallText}</p>
         <h3>{midText}</h3>
         <h3>{largeText1}</h3>
